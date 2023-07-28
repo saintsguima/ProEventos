ac81d3613a7a2e43e11f4bca4b251e40ee35dde0
saintsguima <wladdg@gmail.com>
2023-07-28 10:53:59 -0300

    Seção 2: notNet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Belo Horizonte",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "FOTO.JPG"
+            },
+            new Evento(){
+                EventoId = 2,
+                Tema = "Angular 11 e .NET 5_2",
+                Local = "Mindas",
+                Lote = "2º Lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(4).ToString("dd/MM/yyyy"),
+                ImagemURL = "blablabla.JPG"
+            }
+        };
+
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetBuId(int id)
+        {
+            return _evento.Where(e => e.EventoId == id);  
+        }
+
+    }
+}
